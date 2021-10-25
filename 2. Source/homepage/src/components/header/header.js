f452135e9db20a779f4cb904024cbca2ad75dfe0
vanle <vanle@beeketing.net>
2021-10-26 02:55:02 +0700

    feat(front-page): Create front page

--- 2. Source/homepage/src/components/header/header.js
+++ 2. Source/homepage/src/components/header/header.js
@@ -1,28 +1,14 @@
 import Item from "components/header/item";
 
-const mockdata = [
-    {
-        title: "Trang chủ",
-        link: "www.google.com"
-    },
-    {
-        title: "Trang chủ",
-        link: "www.google.com"
-    },
-    {
-        title: "Trang chủ",
-        link: "www.google.com"
-    },
-]
 const Header = (props) => {
     return (
         <div className="header">
             <div className="header--container">
-                <div className="header--logo">
+                <a href="/" className="header--logo">
                     <img src="/Tmath_logo.png" alt="TMath"/>
                     <span>TMATH</span>
-                </div>
-                {mockdata.map(item => <Item title={item.title} link={item.link} icon={item.icon}/>)}
+                </a>
+                {props.data.map(item => <Item title={item.title} link={item.link} icon={item.icon}/>)}
             </div>
         </div>
     )
